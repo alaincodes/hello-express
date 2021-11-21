5c1881e7302ed7530b3a1009c9ff0f855647756a
Alain Tran <alaint77@gmail.com>
2021-11-21 14:42:30 +0100

    create new route

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('https://ghibliapi.herokuapp.com/films');
+  res.render('about');
 });
 
 module.exports = router;
